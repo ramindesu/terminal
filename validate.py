22805f49e83f008328ebcf1993c11cf1b180270f
ramin <ramindesu88@gmail.com>
2025-10-14 13:34:51 +0330

    FEAT(email validate)

--- validate.py
+++ validate.py
@@ -26,7 +26,7 @@ username_validate = re.compile(r"^[a-zA-Z]{3,20}$")
 password_validate = re.compile(
     r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z1-9]){8,}$"
 )
-
+email_validte = re.compile(r"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")
 
 def validate_username(username):
     if not username_validate.match(username):
@@ -38,3 +38,8 @@ def validate_password(password):
     if not password_validate.match(password):
         raise ValidateError("password is not valid")
     return password
+
+def validate_email(email):
+    if not email_validte.match(email):
+        raise ValidateError("email is not okay")
+    
