7a804fc5220ae7fb3afd0b9af588f55ecf4e2279
ramin <ramindesu88@gmail.com>
2025-10-14 14:32:52 +0330

    ADD(user traveller ticket)

--- classes.py
+++ classes.py
@@ -1,4 +1,19 @@
-
+from validate import *
 class User:
-    def __init__(self):
-        pass
+    def __init__(self,email,username,password):
+        self.email = validate_email(email)
+        self.username = validate_username(username)
+        self.password = validate_password(password)
+class Ticket:
+    pass
+
+class Traveller(User):
+    def __init__(self, email, username, password):
+        super().__init__(email, username, password)
+        self.tickets = []
+
+    def get_ticket(self,ticket:Ticket):
+        if isinstance(ticket) == Ticket:
+            self.tickets.append(ticket)
+        else:
+            raise TypeError("the type is not matched")
