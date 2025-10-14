326a4ef9f9a006c3081fbcca19aaf025fffbdf46
ramin <ramindesu88@gmail.com>
2025-10-14 14:51:22 +0330

    FEAT(journey method)

--- classes.py
+++ classes.py
@@ -1,18 +1,36 @@
 from validate import *
+
+
 class User:
-    def __init__(self,email,username,password):
+    def __init__(self, email, username, password):
         self.email = validate_email(email)
         self.username = validate_username(username)
         self.password = validate_password(password)
+
+
+class Journey:
+    def __init__(self,start,end,status= "pending"):
+            self.start = start 
+            self.end = end
+            self.status = status
+            
+    def change_status(self,status):
+        if status not in ("canceled","done","pending"):
+            raise ChoiseError("only canceled , done, pending can be chosen")
+
+
 class Ticket:
-    pass
+    def __init__(self, journey: Journey, cost):
+        self.journey = journey
+        self.cost = cost
+
 
 class Traveller(User):
     def __init__(self, email, username, password):
         super().__init__(email, username, password)
         self.tickets = []
 
-    def get_ticket(self,ticket:Ticket):
+    def get_ticket(self, ticket: Ticket):
         if isinstance(ticket) == Ticket:
             self.tickets.append(ticket)
         else:
