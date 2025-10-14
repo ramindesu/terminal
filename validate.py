5e3b3ef97de76a3be69c6a74fceb0e551e23c750
ramin <ramindesu88@gmail.com>
2025-10-14 13:18:59 +0330

    ADD(file execptions validate)

---
+++ validate.py
@@ -0,0 +1,40 @@
+
+import re
+
+
+class NotEnoughCash(Exception):
+    pass
+
+
+class JourneyStarted(Exception):
+    pass
+
+
+class ZeroValue(Exception):
+    pass
+
+
+class NotAdmin(Exception):
+    pass
+
+
+class ValidateError(Exception):
+    pass
+
+
+username_validate = re.compile(r"^[a-zA-Z]{3,20}$")
+password_validate = re.compile(
+    r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z1-9]){8,}$"
+)
+
+
+def validate_username(username):
+    if not username_validate.match(username):
+        raise ValidateError("must be letters and upper than 2 charachter")
+    return username
+
+
+def validate_password(password):
+    if not password_validate.match(password):
+        raise ValidateError("password is not valid")
+    return password
