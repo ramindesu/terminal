def3215ee928cd837d14de5a536ece974eb8eaf9
ramin <ramindesu88@gmail.com>
2025-10-22 23:57:20 +0330

    ADD(extra validate)

--- validate.py
+++ validate.py
@@ -1,50 +1,62 @@
-
 import re
 
 
 class NotEnoughCash(Exception):
-    pass
 
+    pass
 
 class JourneyStarted(Exception):
-    pass
 
+    pass
 
 class ZeroValue(Exception):
-    pass
 
+    pass
 
 class NotAdmin(Exception):
-    pass
 
+    pass
 
 class ValidateError(Exception):
+
     pass
 
-class TypeError(Exception):
-    ...
+class InvalidType(Exception):
 
-class ChoiseError(Exception):
-    ...
+    pass
+
+class ChoiceError(Exception):
+
+    pass
+
+
 
 username_validate = re.compile(r"^[a-zA-Z]{3,20}$")
 password_validate = re.compile(
     r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$"
 )
 email_validate = re.compile(r"^[\w.\-]+@([\w\-]+\.)+[\w\-]{2,4}$")
+
+
+
 def validate_username(username):
+
     if not username_validate.match(username):
-        raise ValidateError("must be letters and upper than 2 charachter")
+        raise ValidateError("Username must contain only letters and be at least 3 characters long.")
     return username
 
 
 def validate_password(password):
+
     if not password_validate.match(password):
-        raise ValidateError("password is not valid")
+        raise ValidateError(
+
+        )
     return password
 
+
 def validate_email(email):
+
     if not email_validate.match(email):
-        raise ValidateError("email is not okay")
+        raise ValidateError("Email format is invalid.")
     return email
-    
